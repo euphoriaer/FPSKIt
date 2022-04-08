57a8a0ad5747c67fb9effce5a112dab80a621dd8
Panbin <1764183867@qq.com>
2022-04-08 19:03:57 +0800

    First Finish FPSKit

---
+++ Assets/314 Arts/Weapon Pack Integrations/Infima Games/Low Poly Shooter Pack/Scripts/Kit_LPSP_EventDummies.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// The Low Poly Shooter Pack uses events for its management
+/// Since MMFPSE does not do that, we need a dummy receiver, so it does not throw errors
+/// </summary>
+public class Kit_LPSP_EventDummies : MonoBehaviour
+{
+    public void OnAnimationEndedHolster()
+    {
+
+    }
+
+    public void OnEjectCasing()
+    {
+
+    }
+
+    public void OnAmmunitionFill()
+    {
+
+    }
+
+    public void OnAnimationEndedReload()
+    {
+
+    }
+
+    public void OnSlideBack()
+    {
+
+    }
+
+    public void OnAnimationEndedBolt()
+    {
+
+    }
+
+    public void OnSetActiveMagazine()
+    {
+
+    }
+}
